764aa75baf78b542c56f0fe4c3c32de17ddcce95
chintala naga venkata ajay kumar <chnvakumarofficial@gmail.com>
2025-07-28 23:41:13 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001', // Your backend server URL
+  baseURL: import.meta.env.VITE_BACKEND_URL, // Your backend server URL
 });
 
 export default api;
