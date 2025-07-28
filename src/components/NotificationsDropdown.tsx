a89a8ab32134b8f807f509cc6dc5ba6d5f5a4d10
chintala naga venkata ajay kumar <chnvakumarofficial@gmail.com>
2025-07-28 23:35:24 +0530

    Update NotificationsDropdown.tsx

--- src/components/NotificationsDropdown.tsx
+++ src/components/NotificationsDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import api from '../services/api';
 import { Link } from 'react-router-dom';
