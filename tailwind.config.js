6ce808fa998fdab5a30a11432ad79f3265fadd12
chintala naga venkata ajay kumar <chnvakumarofficial@gmail.com>
2025-07-29 21:46:27 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// 1. tailwind.config.js (in your project's root)
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,4 +9,12 @@ export default {
     extend: {},
   },
   plugins: [],
+}
+
+// 2. postcss.config.js (in your project's root)
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
