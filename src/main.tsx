4076caf37b7c2b0942818dbe5f9d9dfb7976b2f4
chintala naga venkata ajay kumar <chnvakumarofficial@gmail.com>
2025-07-29 16:05:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ import SettingsPage from './pages/SettingsPage.tsx'; // Import the SettingsPage
 import ProtectedRoute from './components/ProtectedRoute.tsx';
 
 // Import CSS
-import './output.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
